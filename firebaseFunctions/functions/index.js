2deaf45b5c2d06dcf7eeb1859b12ab02bc7b5b62
Jacob Menashi <jbmenashi@gmail.com>
2020-05-29 12:36:43 -0500

    auth middleware

--- firebaseFunctions/functions/index.js
+++ firebaseFunctions/functions/index.js
@@ -39,11 +39,43 @@ app.get('/posts', (req, res) => {
     .catch(err => console.error(err)); 
 })
 
+// Middleware for authorization
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1]
+    }
+    else {
+        console.error('No token found')
+        return res.status(403).json({error: 'Unauthorized'})
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken
+            console.log(decodedToken)
+            return db.collection('users').where('userId', '==', req.user.uid).limit(1).get()
+            // return db.collection('users').find(user => user.userId === req.user.uid).get() - this doesn't work
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token', err)
+            return res.status(403).json(err)
+        })
+}
+
 // Create Post
-app.post('/post', (req, res) => {
+app.post('/post', FBAuth, (req, res) => {
+    if (req.body.postText.trim() === '') {
+        return res.status(400).json({postText: 'postText must not be empty'})
+    }
+
     const newPost = {
         postText: req.body.postText,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         // createdAt: admin.firestore.Timestamp.fromDate(new Date())
         createdAt: new Date().toISOString()
     }
