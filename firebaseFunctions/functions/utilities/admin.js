a6a417170d952291af5d575078130a74e85ad1a8
Jacob Menashi <jbmenashi@gmail.com>
2020-06-01 13:47:10 -0500

    new private key

--- firebaseFunctions/functions/utilities/admin.js
+++ firebaseFunctions/functions/utilities/admin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require("./adminObj.json");
+const serviceAccount = require("./xxy.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
